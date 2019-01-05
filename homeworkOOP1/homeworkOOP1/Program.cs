e74b06afe1da11ca5b08df2d5eaeaca23d024927
OanaBlanaru <oana_miha_b@yahoo.com>
2019-01-05 19:01:07 +0200

    homework OOP, first exercise

---
+++ homeworkOOP1/homeworkOOP1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace homeworkOOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! This is my git repo");
+        }
+    }
+}
